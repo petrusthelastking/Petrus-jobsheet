8eb53579ffa75972f7a2319b340d72bb3f7bb170
petrus <petruskmkz@gmail.com>
2023-09-25 12:18:43 +0700

    TUGAS PERTAMA SAYA

---
+++ PemilihanPercobaan22.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class PemilihanPercobaan22 {
+    public static void main(String[] args) {
+        Scanner input00 = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = input00.nextInt();
+        String jenisBilangan = ( angka % 2 == 0) ? "genap" : "ganjil";
+        System.out.println("angka" + angka + " angka anda bersifat" + jenisBilangan);
+    }
+}
+
