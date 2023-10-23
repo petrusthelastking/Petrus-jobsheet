13901e7acb629853974894c61ce6ee813a988f0a
petrusthelastking <143620112+petrusthelastking@users.noreply.github.com>
2023-10-23 10:11:21 +0700

    Arry Bilangan pertanyaan 1

---
+++ MEET 9/ArrayBilangan22Pertanyaan.java
@@ -0,0 +1,13 @@
+public class ArrayBilangan22 {
+    public static void main(String[] args) {
+        double[] bil = new double[4];
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
